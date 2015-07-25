4a43b966b5b88bc4d525fba5c32aa9a74c4cc255
Marc Lobato <marc@ideasdigitales.es>
2015-07-25 13:22:42 +0200

    * nvweb_prepare_link: don't add prefix to "#" urls

--- web/nvweb_routes.php
+++ web/nvweb_routes.php
@@ -210,8 +210,10 @@ function nvweb_load_website_by_url($url, $exit=true)
 
 function nvweb_prepare_link($path)
 {
-    $url = '#';
-    if(substr($path, 0, 7)=='http://' || substr($path, 0, 8)=='https://')
+    if(	substr($path, 0, 7)=='http://' ||
+		substr($path, 0, 8)=='https://' ||
+		substr($path, 0, 1)=='#'
+	)
         $url = $path;
     else
         $url = NVWEB_ABSOLUTE.$path;
