2f32a6e850cf26a184cf610cb2ac4b3c8d408cd5
Navigate CMS <info@navigatecms.com>
2016-09-04 08:47:22 +0200

    * updated tinyMCE v4.4.3

--- lib/packages/about/about.php
+++ lib/packages/about/about.php
@@ -46,7 +46,7 @@ function about_layout()
 	$navibars->add_tab_content_row(
 		array(
 			'<label>'.t(218, 'Third party libraries').'</label>',
-			'<a href="http://www.tinymce.com" target="_blank">TinyMCE 4.3.13</a><br />'
+			'<a href="http://www.tinymce.com" target="_blank">TinyMCE 4.4.3</a><br />'
 		)
 	);
 
