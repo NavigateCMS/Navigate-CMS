109b2ff6616a744a852557f47c8508f2ca2b3c7d
Navigate CMS <info@navigatecms.com>
2016-09-25 13:36:28 +0200

    * properties: find coordinates by map now uses Leaflet script and OpenStreetMaps data (removed Google Maps integration)

--- lib/packages/properties/property.layout.php
+++ lib/packages/properties/property.layout.php
@@ -383,7 +383,7 @@ function navigate_property_layout_field($property, $object="", $website_id="")
 			$field[] = $naviforms->textfield("property-".$property->id.'-longitude', $longitude, '182px');
 			$field[] = '<img src="img/icons/silk/map_magnify.png" align="absmiddle" hspace="3px" id="property-'.$property->id.'-show" />';
 			$field[] = '<div id="property-'.$property->id.'-map-container" style=" display: none; ">';
-			$field[] = '	<div class="navigate-form-row" id="property-'.$property->id.'-search" style=" width: 320px; height: 24px; margin-top: 5px; margin-left: 85px; position: absolute; z-index: 100; opacity: 0.9; ">';
+			$field[] = '	<div class="navigate-form-row" id="property-'.$property->id.'-search" style=" width: 320px; height: 24px; margin-top: 5px; margin-left: 85px; position: absolute; z-index: 1000; opacity: 0.9; ">';
 			$field[] = '		<input type="text" name="property-'.$property->id.'-search-text" style=" width: 280px; " /> ';
 			$field[] = '		<img class="ui-widget ui-button ui-state-default ui-corner-all" sprite="false" style=" cursor: pointer; padding: 3px; " src="'.NAVIGATE_URL.'/img/icons/silk/zoom.png" align="right" />';			
 			$field[] = '	</div>';
@@ -396,11 +396,10 @@ function navigate_property_layout_field($property, $object="", $website_id="")
 					$helper_text = $object->t($helper_text);
 				$field[] = '<div class="subcomment">'.$helper_text.'</div>';
 			}
-			$field[] = '</div>';		
-			$field[] = '<script language="javascript" type="text/javascript" src="https://maps.google.com/maps/api/js"></script>';
+			$field[] = '</div>';
 
 			$layout->add_script('
-				var property_'.$property->id.'_gmap = null;
+				var property_'.$property->id.'_lmap = null;
 			    var marker = null;
 							
 				$("#property-'.$property->id.'-search input").on("keyup", function(e)
@@ -410,41 +409,54 @@ function navigate_property_layout_field($property, $object="", $website_id="")
 				
 				$("#property-'.$property->id.'-show").on("click", function()
 				{
-					var myLatlng = new google.maps.LatLng(
+					var myLatlng = new L.LatLng(
 					    $("#property-'.$property->id.'-latitude").val(),
 					    $("#property-'.$property->id.'-longitude").val()
 					);
-					var myOptions = {
-						zoom: 16,
-						center: myLatlng,
-						mapTypeId: google.maps.MapTypeId.ROADMAP,
-						disableDoubleClickZoom: true
-					};					
-					property_'.$property->id.'_gmap = new google.maps.Map($("#property-'.$property->id.'-map")[0], myOptions);
-					var marker = new google.maps.Marker({
-						  position: myLatlng,
-						  title: myLatlng.lat() + ", " + myLatlng.lng()
-					});
-					marker.setMap(property_'.$property->id.'_gmap);  
-					google.maps.event.addListener(property_'.$property->id.'_gmap, "dblclick", function(event) 
+									
+					// initialize leaflet map
+					property_'.$property->id.'_lmap = L.map(
+					    "property-'.$property->id.'-map",
+					    {
+					        center: myLatlng,
+					        zoom: 17,
+					        doubleClickZoom: false
+					    }
+                    );					    
+                    
+                    // create the tile layer with correct attribution
+	                var osmUrl = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+	                var osmAttrib = "Map data © <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors";
+	                var osm = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 19, attribution: osmAttrib });
+	                
+	                property_'.$property->id.'_lmap.addLayer(osm);
+	                
+	                marker = L.marker(myLatlng).addTo(property_'.$property->id.'_lmap);
+	                
+	                property_'.$property->id.'_lmap.on("dblclick", function(e)
+	                {	                    
+                        $("#property-'.$property->id.'-latitude").val(e.latlng.lat);
+						$("#property-'.$property->id.'-longitude").val(e.latlng.lng)
+	                    
+	                    marker.remove();
+	                    marker = L.marker(e.latlng).addTo(property_'.$property->id.'_lmap);                        
+	                });
+					                    
+					$("#property-'.$property->id.'-map-container").dialog(
 					{
-						marker.setMap(null);
-
-						$("#property-'.$property->id.'-latitude").val(event.latLng.lat());
-						$("#property-'.$property->id.'-longitude").val(event.latLng.lng())
-						marker = new google.maps.Marker({
-						  position: event.latLng,
-						  title: event.latLng.lat() + ", " + event.latLng.lng()
-						});
-						marker.setMap(property_'.$property->id.'_gmap);
-					});
-
-					$("#property-'.$property->id.'-map-container").dialog({
 						width: 600,
 						height: 400,
 						title: "'.t(300, 'Map').': '.t(301, 'Double click a place to set the coordinates').'",
-						resize: property'.$property->id.'resize
-					}).dialogExtend(
+						resize: property'.$property->id.'resize,
+						close: function()
+						{
+                            property_'.$property->id.'_lmap.remove();				    
+						},
+						open: function()
+						{
+						    property_'.$property->id.'_lmap.invalidateSize();
+                        }
+                    }).dialogExtend(
 					{
 						maximizable: true,
 						"maximize" : property'.$property->id.'resize,
@@ -459,58 +471,28 @@ function navigate_property_layout_field($property, $object="", $website_id="")
 				{
 					$("#property-'.$property->id.'-map").width($("#property-'.$property->id.'-map-container").width()); 
 					$("#property-'.$property->id.'-map").height($("#property-'.$property->id.'-map-container").height());	
-					google.maps.event.trigger(property_'.$property->id.'_gmap, "resize");
+					property_'.$property->id.'_lmap.invalidateSize();
 				}
 				
 				function property'.$property->id.'search()
 				{				
 					var address = $("#property-'.$property->id.'-search input").val();
-					var geocoder = new google.maps.Geocoder();
-					geocoder.geocode( { "address": address}, function(results, status) 
-					{
-						if(status == google.maps.GeocoderStatus.OK)
-						{
-							property_'.$property->id.'_gmap.setCenter(results[0].geometry.location);
-							var marker = new google.maps.Marker(
-							{
-								map: property_'.$property->id.'_gmap, 
-								position: results[0].geometry.location
-							});							
-						} 
-						else 
-						{
-						    if(status=="REQUEST_DENIED") // Api limit reached?
-						    {
-						        // try to use an alternative geocode service
-                                $.getJSON("http://services.gisgraphy.com/geocoding/geocode?format=json&callback=?&address="+address, function(data)
-                                {                                    
-                                    if(!data.result || data.result.length < 1)
-                                        alert("Geocode was not successful for the following reason: " + status);
-                                    else
-                                    {
-                                        var myLatLng = new google.maps.LatLng(data.result[0].lat, data.result[0].lng);
-                                        
-                                        /*
-                                        var marker = new google.maps.Marker(
-							            {
-								            map: property_'.$property->id.'_gmap, 
-								            position: myLatLng
-							            });
-							            */
-							                   
-                                        property_'.$property->id.'_gmap.setCenter(myLatLng);
-                                    }
-                                });						        
-						    }
-						    else
-						        alert("Geocode was not successful for the following reason: " + status);
-						}
-					});
-					
+
+                    $.getJSON("http://services.gisgraphy.com/geocoding/geocode?format=json&callback=?&address="+address, function(data)
+                    {                                    
+                        if(!data.result || data.result.length < 1)
+                            alert("Geocode was not successful for the following reason: " + status);
+                        else
+                        {
+                            property_'.$property->id.'_lmap.setView([data.result[0].lat, data.result[0].lng], 19);
+                        }
+                    });						        
+
 					return false;
-				}				
+				}		
+                
 			');
-			break;		
+			break;
 			
 		case 'text':
 			foreach($langs as $lang)
