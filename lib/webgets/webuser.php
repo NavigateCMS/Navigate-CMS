3ef2233a52c0b11a70e7bb74d55b8c2a7c3144e9
Navigate CMS <info@navigatecms.com>
2016-10-31 20:23:45 +0100

    + nvweb webuser: new modes "forgot_password", "gender" and "newsletter"

--- lib/webgets/webuser.php
+++ lib/webgets/webuser.php
@@ -28,7 +28,9 @@ function nvweb_webuser($vars=array())
             'subscribe_error' => t(542, 'There was a problem subscribing your email to the newsletter.'),
             'email_confirmation' => t(454, "An e-mail with a confirmation link has been sent to your e-mail account."),
             'click_to_confirm_account' => t(607, "Click on the link below to confirm your account"),
-            'email_confirmation_notice' => t(608, "This is an automated e-mail sent as a result of a newsletter subscription request. If you received this e-mail by error just ignore it.")
+            'email_confirmation_notice' => t(608, "This is an automated e-mail sent as a result of a newsletter subscription request. If you received this e-mail by error just ignore it."),
+            'forgot_password_success' => t(648, "An e-mail with a temporary password has been sent to your e-mail account."),
+            'forgot_password_error' => t(446, "We're sorry. Your contact request could not be sent. Please try again or find another way to contact us.")
         );
 
         // theme translations
@@ -37,9 +39,11 @@ function nvweb_webuser($vars=array())
             "login_incorrect": "Login incorrect.",
             "subscribed_ok": "Your email has been successfully subscribed to the newsletter.",
             "subscribe_error": "There was a problem subscribing your email to the newsletter.",
-            "email_confirmation" => "An e-mail with a confirmation link has been sent to your e-mail account.",
-            "click_to_confirm_account" => "Click on the link below to confirm your account",
-            "email_confirmation_notice" => "This is an automated e-mail sent as a result of a newsletter subscription request. If you received this e-mail by error just ignore it."
+            "email_confirmation": "An e-mail with a confirmation link has been sent to your e-mail account.",
+            "click_to_confirm_account": "Click on the link below to confirm your account",
+            "email_confirmation_notice": "This is an automated e-mail sent as a result of a newsletter subscription request. If you received this e-mail by error just ignore it."
+            "forgot_password_success": "An e-mail with a temporary password has been sent to your e-mail account.",
+            "forgot_password_error": "We're sorry. Your contact request could not be sent. Please try again or find another way to contact us."
         */
         if(!empty($website->theme) && method_exists($theme, 't'))
         {
@@ -70,6 +74,15 @@ function nvweb_webuser($vars=array())
         case 'fullname':
             if(!empty($webuser->fullname))
                 $out = $webuser->fullname;
+            break;
+
+        case 'gender':
+            if(!empty($webuser->gender))
+                $out = $webuser->gender;
+            break;
+
+        case 'newsletter':
+            $out = $webuser->newsletter;
             break;
 
         case 'email':
@@ -122,12 +135,163 @@ function nvweb_webuser($vars=array())
                     }
                 }
                 else
+                {
                     $webuser->set_cookie();
+                    if(!empty($vars['notify']))
+                    {
+                        if($vars['notify']=='callback')
+                            nvweb_after_body('js', $vars['callback'].'(true);');
+                    }
+                }
             }
             break;
 
         case 'signout_link':
             $out = NVWEB_ABSOLUTE.$website->homepage().'?webuser_signout';
+            break;
+
+        case 'forgot_password':
+            // pre checks: correct form, not spambot, email not empty and valid
+            // load the associated user account
+            // create temporary password and send email
+            // TODO: don't change the password, just generate a link and let the user enter their preferred new password
+
+            // a page may have several forms, which one do we have to check?
+            if(!empty($vars['form']))
+            {
+                list($field_name, $field_value) = explode('=', $vars['form']);
+                if($_POST[$field_name]!=$field_value)
+                    return;
+            }
+
+            // check if this send request really comes from the website and not from a spambot
+            if( parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) != $website->subdomain.'.'.$website->domain &&
+                parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) != $website->domain )
+                return;
+
+            if(empty($vars['email_field']))
+                $vars['email_field'] = 'newsletter_email';
+
+            $email = $_REQUEST[$vars['email_field']];
+            $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+            if(!empty($vars['email_field']) && !empty($email))
+            {
+                $ok = false;
+
+                if(filter_var($email, FILTER_VALIDATE_EMAIL)!==FALSE)
+                {
+                    $wu_id = $DB->query_single(
+                        'id',
+                        'nv_webusers',
+                        ' email = '.protect($email).'
+                          AND website = '.$website->id
+                    );
+
+                    $wu = new webuser();
+
+                    if(!empty($wu_id))
+                    {
+                        $wu->load($wu_id);
+                        if( $wu->access == 0 ||
+                            ( $wu->access == 2 &&
+                                ($wu->access_begin==0 || time() > $wu->access_begin) &&
+                                ($wu->access_end==0 || time() < $wu->access_end)
+                            )
+                        )
+                        {
+                            // generate new password
+                            $password = generate_password(8, false, 'luds');
+                            $wu->set_password($password);
+                            $ok = $wu->save();
+
+                            // send a message to communicate the new webuser's email
+                            $message = navigate_compose_email(array(
+                                array(
+                                    'title' => $website->name,
+                                    'content' => t(451, "This is an automated e-mail sent as a result of a password request process. If you received this e-mail by error just ignore it.")
+                                ),
+                                array(
+                                    'title' => t(1, "User"),
+                                    'content' => $wu->username
+                                ),
+                                array(
+                                    'title' => t(2, "Password"),
+                                    'content' => $password
+                                ),
+                                array(
+                                    'footer' => '<a href="'.$website->absolute_path().$website->homepage().'">'.$website->name.'</a>'
+                                )
+                            ));
+
+                            @nvweb_send_email($website->name, $message, $wu->email);
+                        }
+                    }
+                }
+
+                if($ok)
+                    $message = $webgets[$webget]['translations']['forgot_password_success'];
+                else
+                    $message = $webgets[$webget]['translations']['forgot_password_error'];
+
+                if(empty($vars['notify']))
+                    $vars['notify'] = 'inline';
+
+                switch($vars['notify'])
+                {
+                    case 'alert':
+                        nvweb_after_body('js', 'alert("'.$message.'");');
+                        break;
+
+                    case 'inline':
+                        if($ok)
+                            $out = '<div class="nvweb-forgot-password-form-success">'.$message.'</div>';
+                        else
+                            $out = '<div class="nvweb-forgot-password-form-error">'.$message.'</div>';
+                        break;
+
+                    case 'boolean':
+                        $out = $ok;
+                        break;
+
+                    case 'false':
+                        break;
+
+                    // javascript callback
+                    case 'callback':
+                    default:
+                        if($ok)
+                            nvweb_after_body('js', $vars['callback'].'("'.$message.'");');
+                        else
+                        {
+                            if(!empty($vars['error_callback']))
+                                nvweb_after_body('js', $vars['error_callback'].'("'.$message.'");');
+                            else
+                                nvweb_after_body('js', $vars['callback'].'("'.$message.'");');
+                        }
+                        break;
+                }
+
+            }
+
+            break;
+
+        case 'signup':
+            // TODO
+            // pre checks: correct form, not spambot, email not empty and valid
+            // get the profile data from the form
+            // more checks: password strenght & confirmation, etc.
+            // save the new webuser account
+            // prepare account confirmation (unless not required by webget attributes)
+            //      leave the account blocked
+            //      generate an activation key
+            //      send confirmation email
+            // if no account confirmation is required, auto login
+
+            break;
+
+        case 'avatar':
+            // TODO return webuser's avatar image
             break;
 
         case 'newsletter_subscribe':
@@ -140,7 +304,8 @@ function nvweb_webuser($vars=array())
             }
 
             // check if this send request really comes from the website and not from a spambot
-            if(parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) != $website->subdomain.'.'.$website->domain)
+            if( parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) != $website->subdomain.'.'.$website->domain &&
+                parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) != $website->domain )
                 return;
 
 
