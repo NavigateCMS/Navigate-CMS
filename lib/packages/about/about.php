b0118878a7fe3b664b8ec2bc993dfcb670b99e60
Navigate CMS <info@navigatecms.com>
2017-04-22 10:35:53 +0200

    * add some untracked tinymce4 files into repository (fixed rules in .hgignore)

--- lib/packages/about/about.php
+++ lib/packages/about/about.php
@@ -82,8 +82,12 @@ function about_layout()
 		)
 	);
 
-	$navibars->add_tab_content_row(array(	'<label>&nbsp;</label>',
-											'<a href="https://github.com/josh18/TinyMCE-FontAwesome-Plugin" target="_blank">TinyMCE Font Awesome plugin v2.0.8_nv</a><br />' ));
+	$navibars->add_tab_content_row(
+		array(
+			'<label>&nbsp;</label>',
+			'<a href="https://github.com/josh18/TinyMCE-FontAwesome-Plugin" target="_blank">TinyMCE Font Awesome plugin v2.0.9_nv</a><br />'
+		)
+	);
 
 	$navibars->add_tab_content_row(array(	'<label>&nbsp;</label>',
 											'<a href="https://github.com/maschek/imgmap" target="_blank">TinyMCE imgmap plugin v1.09</a><br />' ));
