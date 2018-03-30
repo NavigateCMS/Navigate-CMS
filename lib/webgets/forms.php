b1152b00b68898ef1c5c4f57fbd0a0c8dcfa1eac
Navigate CMS <info@navigatecms.com>
2018-03-30 12:07:10 +0200

    * nvweb forms: improved default selected value in country_selector and country_region_selector

--- lib/webgets/forms.php
+++ lib/webgets/forms.php
@@ -17,15 +17,15 @@ function nvweb_forms($vars=array())
             array_unshift($country_codes, '');
             array_unshift($country_names, '('.t(307, "Unspecified").')');
 
+            $default = $vars['default'];
+            if(isset($_REQUEST[$vars['field_name']]) && !empty($_REQUEST[$vars['field_name']]))
+                $default = $_REQUEST[$vars['field_name']];
+
             $out[] = '<select name="'.$vars['field_name'].'" id="'.$vars['field_id'].'" class="'.$vars['class'].'">';
             for($c=0; $c < count($country_codes); $c++)
             {
-                if( $_REQUEST[$vars['field_name']] == $country_codes[$c]  ||
-                    (!isset($_REQUEST[$vars['field_name']]) && $vars['default'] == $country_codes[$c])
-                )
-                {
+                if( $country_codes[$c] == $default )
                     $out[] = '<option value="'.$country_codes[$c].'" selected>'.$country_names[$c].'</option>';
-                }
                 else
                     $out[] = '<option value="'.$country_codes[$c].'">'.$country_names[$c].'</option>';
             }
@@ -39,14 +39,15 @@ function nvweb_forms($vars=array())
 
             $out[] = '<select name="'.$vars['field_name'].'" id="'.$vars['field_id'].'" class="'.$vars['class'].'" data-country-field="'.$vars['country_field'].'">';
             $out[] = '<option data-country="" value="">('.t(307, "Unspecified").')</option>';
+
+            $default = $vars['default'];
+            if(isset($_REQUEST[$vars['field_name']]) && !empty($_REQUEST[$vars['field_name']]))
+                $default = $_REQUEST[$vars['field_name']];
+
             for($r = 0; $r < count($regions); $r++)
             {
-                if( $_REQUEST[$vars['field_name']] == $regions[$r]->region_id  ||
-                    (!isset($_REQUEST[$vars['field_name']]) && $vars['default'] == $regions[$r]->region_id)
-                )
-                {
+                if( $regions[$r]->region_id  == $default )
                     $out[] = '<option data-country="'.$regions[$r]->country_code.'" value="'.$regions[$r]->region_id.'" selected>'.$regions[$r]->name.'</option>';
-                }
                 else
                     $out[] = '<option data-country="'.$regions[$r]->country_code.'" value="'.$regions[$r]->region_id.'">'.$regions[$r]->name.'</option>';
             }
@@ -58,13 +59,17 @@ function nvweb_forms($vars=array())
                     nvweb_after_body('html', '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>');
 
                 nvweb_after_body('js', '
-                    $("select[name='.$vars['country_field'].']").on("change", function()
+                    $("select[name='.$vars['country_field'].']").on("change", function(event, keep_selected)
                     {
                         var that = this;
-                        $("select[name='.$vars['field_name'].']").find("option:selected").removeAttr("selected");
+                        if(!keep_selected)
+                            $("select[name='.$vars['field_name'].']").find("option:selected").removeAttr("selected");
                         $("select[name='.$vars['field_name'].']").find("option").not(":first").hide();
-                        $("select[name='.$vars['field_name'].']").find("option[data-country="+$(that).find("option:selected").val()+"]").show();
+                        if($(that).find("option:selected").val()!="")
+                            $("select[name='.$vars['field_name'].']").find("option[data-country="+$(that).find("option:selected").val()+"]").show();
                     });
+                    
+                    $("select[name='.$vars['country_field'].']").trigger("change", [true]);
                 ');
             }
 
