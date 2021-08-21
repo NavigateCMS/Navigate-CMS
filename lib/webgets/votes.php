46a9a4121442827bbe6d88fdf7f51554cbb9c54a
Navigate CMS <info@navigatecms.com>
2021-08-21 17:04:06 +0200

    + nvweb votes: added mode "percentage" (score from 0 to 100 with 2 decimals, based on score from 0 to 10)

--- lib/webgets/votes.php
+++ lib/webgets/votes.php
@@ -11,6 +11,25 @@ function nvweb_votes($vars=array())
 		case 'score':
             $out = nvweb_votes_calc($current['object'], $vars['round'], $vars['half'], $vars['min'], $vars['max']);
 			break;
+
+        case 'percentage':
+            $out = nvweb_votes_calc($current['object'], false, false);
+            // returns score from 0 to 10, with decimals
+            $out = $out * 10;
+            $out = round($out, 2, PHP_ROUND_HALF_UP);
+
+            if(!empty($vars['max']) && $out > $vars['max'])
+            {
+                $out = $vars['max'];
+            }
+
+            if(!empty($vars['min']) && $out < $vars['min'])
+            {
+                $out = $vars['min'];
+            }
+
+            $out = number_format($out, 2, '.', '');
+            break;
 			
 		case 'votes':
 			$out = $current['object']->votes;
@@ -48,10 +67,14 @@ function nvweb_votes_calc($object, $round="", $half=false, $min="", $max="")
     $score = 0;
 
     if($object->votes > 0)
+    {
         $score = $object->score / $object->votes;
+    }
 
     if($half=='true')
+    {
         $score = $score / 2;
+    }
 
     switch($round)
     {
@@ -63,18 +86,30 @@ function nvweb_votes_calc($object, $round="", $half=false, $min="", $max="")
             $out = ceil($score);
             break;
 
+        case false:
+            $out = $score;
+            break;
+
         default:
             if(is_numeric($round))
+            {
                 $out = round($score, $round);
+            }
             else
+            {
                 $out = $score;
+            }
     }
 
     if(!empty($max) && $out > $max)
+    {
         $out = $max;
+    }
 
     if(!empty($min) && $out < $min)
+    {
         $out = $min;
+    }
 
     return $out;
 }
