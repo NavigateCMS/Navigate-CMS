fabb4718d7c6a4e0bdf02800d55d7de2cf492261
Navigate CMS <info@navigatecms.com>
2021-11-28 17:36:30 +0100

    * navigate_download: prevent arbitrary file read vulnerability

--- navigate_download.php
+++ navigate_download.php
@@ -60,11 +60,29 @@ if(!empty($_REQUEST['id']))
         // note: if the "id" parameter is not numeric, then it could be an external URL request
         $url = $_REQUEST['id'];
         $url = filter_var($url, FILTER_SANITIZE_URL);
+
         // disallow use of < > chars in a URL
         $url = str_replace(array('<', '>'), '', $url);
+
         // prevent directory traversal
-        $url = core_remove_directory_traversal($url);
-        $item->load($url);
+        $url_dtr = core_remove_directory_traversal($url);
+        if($url != $url_dtr)
+        {
+            $url = "";
+        }
+
+        // make sure it is an external URL
+        if( strpos($url, 'http://')===0 ||
+            strpos($url, 'https://')===0
+        )
+        {
+            $item->load($url);
+        }
+        else
+        {
+            header("HTTP/1.1 404 Not Found");
+            core_terminate();
+        }
     }
 }
 
